4d5f92ec846184cb49a7784082c1e971b8871484
Postolache Daniel <postolache.daniel02@gmail.com>
2026-09-15 10:10:13 +0300

    Add files via upload

---
+++ functia_print_15.09.26.py
@@ -0,0 +1,8 @@
+a=5
+b=4
+print('suma ', a, '+', b,'=',a+b, end='\n')
+print('produsul ', a, '*', b,'=',a*b, end='\n')
+print('diferenta ', a, '-', b,'=',a-b, end='\n')
+print('puterea ', a, '**', b, '=', a**b, end='\n')
+print('câtul împărțirii ', a, '//', b, '=', a//b, end='\n')
+print('restul împărțirii ', a, '%', b, '=', a%b, end='\n')
